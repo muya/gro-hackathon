1a58a47f71b1943a4e8ad497f9892216d8b51929
Muya <kingkonig@gmail.com>
2016-03-06 19:33:16 +0300

    Wrap DB set up in transaction

--- file_processor.py
+++ file_processor.py
@@ -35,8 +35,9 @@ class NassJSONDataProcessor(object):
                 dbname, user=dbuser, password=dbpass, host=dbhost, port=dbport)
             self.db.connect()
             models.database_proxy.initialize(self.db)
-            self.db.drop_table(models.FactData, fail_silently=True)
-            self.db.create_table(models.FactData)
+            with self.db.atomic as txn:
+                self.db.drop_table(models.FactData, fail_silently=True)
+                self.db.create_table(models.FactData)
             print "Database initialized successfully"
         except Exception, e:
             print "Sorry, an error occurred while connecting to the db: %s" % e
